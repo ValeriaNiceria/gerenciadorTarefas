a0cabf4f4f02f8cb0151fb1dc47c956b0594fd9d
ValeriaNiceria <valerianiceria@gmail.com>
2018-03-17 09:53:13 -0300

    Criado o htaccess

--- application/views/includes/menu.php
+++ application/views/includes/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?= base_url('dashboard')?>">Grenciador de tarefas</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?= base_url()?>">Grenciador de tarefas</a>
     <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
         <div class="row">
@@ -16,7 +16,7 @@
         <div class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
-            <a class="nav-link active" href="<?= base_url('dashboard')?>">
+            <a class="nav-link active" href="<?= base_url()?>">
                 <span data-feather="home"></span>
                 Dashboard <span class="sr-only">(current)</span>
             </a>
