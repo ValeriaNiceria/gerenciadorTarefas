a9d575ab82ba077fcbdc09c2f6ff92155fa3b9bf
ValeriaNiceria <valerianiceria@gmail.com>
2018-03-22 09:33:05 -0300

    Iniciando validação com jquery - cadastro tarefa

--- application/views/tarefa/atualizar.php
+++ application/views/tarefa/atualizar.php
@@ -1,15 +1,31 @@
-<form action="<?= base_url('tarefa/atualizar')?>" method="post">
-    <div class="form-group">
+<form action="<?= base_url('tarefa/atualizar')?>" method="post" class="needs-validation" novalidate>
+
+    <div class="form-row">
+      <div class="col-md-12 mb-3">
         <label for="titulo">Título:</label>
-        <input type="text" class="form-control" id="titulo" name="titulo" value="<?= $tarefa['titulo']?>" placeholder="Título da tarefa" autofocus required/>
+         <input type="text" class="form-control" id="titulo" name="titulo" value="<?= $tarefa['titulo']?>" placeholder="Título da tarefa" autofocus required/>
+
+        <div class="invalid-tooltip">
+          Informe um título para tarefa.
+        </div>
+
+      </div>
     </div>
 
-    <div class="form-group">
+    <div class="form-row">
+      <div class="col-md-12 mb-3">
         <label for="descricao">Descrição:</label>
         <textarea class="form-control" id="descricao" name="descricao" placeholder="Descrição da tarefa" required><?= $tarefa['descricao']?></textarea>
+        
+        <div class="invalid-tooltip">
+          Informe uma descrição para tarefa.
+        </div>
+
+      </div>
     </div>
 
-    <div class="form-group">
+    <div class="form-row">
+      <div class="col-md-12 mb-3">
         <label for="prioridade">Prioridade:</label>
         <select name="prioridade" id="prioridade" class="custom-select" required>
             <option value="" selected disabled>Escolher...</option>
@@ -17,9 +33,35 @@
             <option value="2" <?= ($tarefa['prioridade'] == 2) ? "selected" : "";?>>Normal</option>
             <option value="3" <?= ($tarefa['prioridade'] == 3) ? "selected" : "";?>>Alta</option>
         </select>
+
+        <div class="invalid-tooltip">
+          Selecione um nível de prioridade.
+        </div>
+      </div>  
     </div>
 
     <input type="hidden" name="id" value="<?= $tarefa['id']?>"/>
 
-    <button type="submit" class="btn btn-success float-right"><span data-feather="save"></span>Salvar atualização</button>
+    <button type="submit" class="btn btn-success float-right"><span data-feather="save"></span>Salvar</button>
 </form>
+
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
--- application/views/tarefa/cadastro.php
+++ application/views/tarefa/cadastro.php
@@ -1,71 +1,68 @@
-<form action="<?= base_url('tarefa/adicionar')?>" method="post" class="needs-validation" novalidate>
+<form action="<?= base_url('tarefa/adicionar')?>" method="post">
 
-    <div class="form-row">
-      <div class="col-md-12 mb-3">
-        <label for="titulo">Título:</label>
-        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título da tarefa" autofocus required/>
+    <div class="form-group">
+      <label for="titulo">Título:</label>
+      <input type="text" class="form-control" id="titulo" name="titulo" onkeyup="validacaoCadastro()"  placeholder="Título da tarefa" autofocus/>
 
-        <div class="invalid-tooltip">
-          Informe um título para tarefa.
-        </div>
-
-        <div class="valid-tooltip">
-          Parece um bom título!
-        </div>
+      <div id="validacao-titulo" class="validacao badge badge-danger">
+        Informe um título para tarefa.
       </div>
     </div>
 
-    <div class="form-row">
-      <div class="col-md-12 mb-3">
-        <label for="descricao">Descrição:</label>
-        <textarea class="form-control" id="descricao" name="descricao" placeholder="Descrição da tarefa" required></textarea>
-        
-        <div class="invalid-tooltip">
-          Informe uma descrição para tarefa.
-        </div>
-
-        <div class="valid-tooltip">
-          Parece uma boa descrição!
-        </div>
+    <div class="form-group">
+      <label for="descricao">Descrição:</label>
+      <textarea class="form-control" id="descricao" name="descricao" placeholder="Descrição da tarefa"></textarea>
+      
+      <div id="validacao-descricao" class="validacao badge badge-danger">
+        Informe uma descrição para tarefa.
       </div>
     </div>
 
-    <div class="form-row">
-      <div class="col-md-12 mb-3">
-        <label for="prioridade">Prioridade:</label>
-        <select name="prioridade" id="prioridade" class="custom-select" required>
-            <option value="" selected disabled>Escolher...</option>
-            <option value="1">Baixa</option>
-            <option value="2">Normal</option>
-            <option value="3">Alta</option>
-        </select>
+    <div class="form-group">
+      <label for="prioridade">Prioridade:</label>
+      <select name="prioridade" id="prioridade" class="custom-select" required>
+          <option value="" selected disabled>Escolher...</option>
+          <option value="1">Baixa</option>
+          <option value="2">Normal</option>
+          <option value="3">Alta</option>
+      </select>
 
-        <div class="invalid-tooltip">
-          Selecione um nível de prioridade.
-        </div>
+      <div id="validacao-prioridade" class="validacao badge badge-danger">
+        Selecione um nível de prioridade.
       </div>  
     </div>
 
-    <button type="submit" class="btn btn-success float-right"><span data-feather="save"></span>Salvar</button>
+    <button type="submit" class="btn btn-success float-right" onclick="validacaoCadastro()"><div data-feather="save"></div>Salvar</button>
 </form>
 
+
+<!-- Validação dos campos -->
 <script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+  $(document).ready(function() {
+    $("#titulo").keyup(validacaoCadastro); 
+  });
+
+  function validacaoCadastro() {
+    var titulo = $("#titulo").val();
+    var descricao = $("#descricao").val();
+    var prioridade = $("#prioridade").val();
+
+    if (titulo == "")
+    {
+      $("#titulo").css("border-color", "red");
+      $("#titulo").css("background-color", "#FFC6C6");
+      $("#validacao-titulo").show();     
+    } else if (titulo != "") {
+      $("#titulo").css("border-color", "green");
+      $("#validacao-titulo").hide(); 
+    }
+
+    if (descricao == "")
+    {
+      $("#titulo").css("border-color", "red");
+      $("#titulo").css("background-color", "#FFC6C6");
+      $("#validacao-titulo").show();     
+    }
+  }
+    
 </script>
